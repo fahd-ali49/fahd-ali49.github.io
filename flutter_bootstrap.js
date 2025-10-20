acdaa00b24c7651512d4ec5a5b793f786f98da71
fahdbinali <sjil6868@gmail.com>
2025-10-20 16:26:10 +0300

    تحديث الشعار والخط

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "952821487"
+    serviceWorkerVersion: "2541311164"
   }
 });
