587cc711ae4a681d9311a191b6b62677c80baceb
fahdbinali <sjil6868@gmail.com>
2025-10-21 00:55:22 +0300

    تحديث خلفية الحاسبه

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2f201e7d529b913dfda019cdfe2cc18c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "89d5f02ccad3e84a40f87cf8da047be8",
+"flutter_bootstrap.js": "fb183d63069b48eb673449145766f8e0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
