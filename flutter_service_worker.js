24d640918e431f433d57bd01b69f2f1e89cfa6a9
fahdbinali <sjil6868@gmail.com>
2025-10-21 03:28:01 +0300

    ت

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0ce4e605c7eeadc0e36dc3427e95f64c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2aeedc9b0eb71ad4ae151266907c6e70",
+"flutter_bootstrap.js": "e8e0c5869eebc171b058e8f719d9a1eb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
